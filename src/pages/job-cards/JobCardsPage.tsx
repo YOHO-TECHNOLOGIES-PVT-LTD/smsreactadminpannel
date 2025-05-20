21ef5852dfe7a7f2cf3dc4789ee7088ba7424bf5
Vishnu <vishnu@Vishnus-MacBook-Air.local>
2025-05-20 18:03:03 +0530

    created the quatation

--- src/pages/job-cards/JobCardsPage.tsx
+++ src/pages/job-cards/JobCardsPage.tsx
@@ -1,5 +1,4 @@
-import Form1StepperInfo from "./steps/Form1stepperInfo"
-import Form1stepperInfo from "./steps/Form1stepperInfo"
+
 import Quotationpage from "./steps/Quotationpage"
 
 export const JobCardsPage = () => {
