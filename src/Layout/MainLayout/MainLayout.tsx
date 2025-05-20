21afc3a2156a3e5f047661d997edb3540ac26b4a
Sumi-Yoho <sumiofficialyoho@gmail.com>
2025-05-20 17:29:23 +0530

    updating

--- src/Layout/MainLayout/MainLayout.tsx
+++ src/Layout/MainLayout/MainLayout.tsx
@@ -1,26 +1,29 @@
 // import React from 'react';
-import { Outlet } from 'react-router-dom';
-import { Navbar } from '../../components/common/Navbar/Navbar.tsx';
-import { Sidebar } from '../../components/common/Sidebar/Sidebar.tsx';
-import { COLORS } from '../../constants/uiConstants.ts';
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../../components/common/Navbar/Navbar.tsx";
+import { Sidebar } from "../../components/common/Sidebar/Sidebar.tsx";
+import { COLORS } from "../../constants/uiConstants.ts";
 
 export const MainLayout = () => {
-    return (
-        <div className="flex h-screen bg-gray-100">
-            {/* Sidebar takes up 1/9 of the width */}
-            <div className="w-1/9">
-                <Sidebar />
-            </div>
+  return (
+    <div className="flex h-screen bg-gray-100">
+      {/* Sidebar takes up 1/9 of the width */}
+      <div className="w-1/9">
+        <Sidebar />
+      </div>
 
-            {/* Main content takes up the remaining 8/9 of the width */}
-            <div className="flex-1 flex flex-col overflow-hidden">
-                <Navbar />
-                <main className="flex-1 overflow-auto ">
-                    <div className="p-6 rounded shadow" style={{backgroundColor: COLORS.bgColor}}>
-                        <Outlet />
-                    </div>
-                </main>
-            </div>
-        </div>
-    );
+      {/* Main content takes up the remaining 8/9 of the width */}
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Navbar />
+        <main className="flex-1 overflow-auto scrollbar-hide">
+          <div
+            className="p-6 rounded shadow"
+            style={{ backgroundColor: COLORS.bgColor }}
+          >
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 };
