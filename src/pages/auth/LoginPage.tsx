bfab2195fec29d3e30124de6481e3fee3a681f36
ARAVINTH-12345 <aravinth.yoho@gmail.com>
2025-05-20 18:22:25 +0530

    Log in page is created

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,7 +1,41 @@
 
+import carImage from "../../assets/CAR IMAGE.jpg"
+
 const LoginPage = () => {
+
+
   return (
-    <div>LoginPage</div>
+    <div  className="flex h-screen ">
+      
+       <div className="w-1/2 ">
+              <img src={carImage}
+                className="w-full h-full "></img>
+        </div>
+      <div className="w-1/2  ">
+      
+      
+        <div className="flex justify-center p-7   ">
+          <div className="w-full h-full p-5 m-10 place-content-center rounded-t-lg rounded-b-lg bg-[#E6A895]  ">
+            <form className="grid gap-4 ">
+              <p className="text-center text-red-800 font-bold ">LOGIN</p>
+              <label htmlFor="USERNAME" >Usename :</label> 
+              <input type="text" placeholder="enter your name" id="USERNAME" className="w-full border-2 border-gray-300 rounded p-1"></input>
+              <label htmlFor="PASSWORD">Password :</label> 
+              <input type="text" placeholder="enter your password" id="PASSWORD" className="w-full border-2 border-gray-300 rounded p-1"></input> 
+              <div className="text-end">
+                <a href="" >Forget Password?</a>
+              </div>
+              <input type="submit" value="submit" className="w-full  bg-red-800 text-white py-2 px-4 rounded-full hover:bg-blue-500 "></input>
+            </form>
+          </div>
+        </div>
+
+                          
+                         
+                      
+      </div>
+    </div>
+    
   )
 }
 
