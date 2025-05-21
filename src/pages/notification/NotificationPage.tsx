def2b46549129e1bd26ceb2bea29eb46cbec9cec
mugilane.cse <55045435+Mugilane@users.noreply.github.com>
2025-05-21 19:25:38 +0530

    pull code arun

--- src/pages/notification/NotificationPage.tsx
+++ src/pages/notification/NotificationPage.tsx
@@ -2,12 +2,14 @@
 
 import AutomatedNotifications from '../../components/common/Notification/AutomatedNotifications.tsx'
 import NotificationsLogs from '../../components/common/Notification/NotificationsLogs.tsx'
+import SosDashboard from '../sos/SosDashboard.tsx'
 
 export const NotificationPage = () => {
   return (
-    <div>
+    <div className='flex flex-col gap-20'>
       <AutomatedNotifications />
       <NotificationsLogs />
+      <SosDashboard/>
     </div>
 
   )
