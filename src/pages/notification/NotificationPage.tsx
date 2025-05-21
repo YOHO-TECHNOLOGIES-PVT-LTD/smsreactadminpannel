c0fa15768918a84a08786c34045842d5a0240657
suruthiga407! <suruthiga.yaho@gmail.com>
2025-05-21 10:13:58 +0530

    Notificationcustom

--- src/pages/notification/NotificationPage.tsx
+++ src/pages/notification/NotificationPage.tsx
@@ -1,19 +1,13 @@
 // import React from 'react'
 
 import AutomatedNotifications from '../../components/common/Notification/AutomatedNotifications.tsx'
-
+import CustomNotifications from '../../components/common/Notification/CustomNotifications.tsx'
 export const NotificationPage = () => {
   return (
     <div>
-      <AutomatedNotifications/>
+    <AutomatedNotifications/> 
+      <CustomNotifications/> 
     </div>
     
   )
 }
-// export const AutomatedNotificationPage =() =>{
-
-//   return(
-
-//     <> </>
-//   )
-// }
