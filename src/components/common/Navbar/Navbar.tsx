0f7eaecbba84d62052d378fa9df618cbab35a91c
Sumi-Yoho <sumiofficialyoho@gmail.com>
2025-05-22 12:11:37 +0530

    navbar updatinh

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -158,9 +158,20 @@ export const Navbar: React.FC = () => {
           </div>
         </div>
 
-        {/* SOS Icon */}
-        <div className="relative h-screen">
-          <SosButton />
+        {/* SOS Emergency Icon */}
+        <div className="relative w-full">
+          {/* SOS Icon */}
+          <div className="absolute right-4 top-1/2 -translate-y-1/2">
+            <div className="relative">
+              <span className="absolute inline-flex h-8 w-8 rounded-full bg-red-400 opacity-75 animate-ping"></span>
+              <button
+                onClick={handleSosClick}
+                className="relative z-10 inline-flex items-center justify-center w-10 h-10 rounded-full bg-gradient-to-r from-red-600 to-red-800 text-white font-bold text-sm shadow-lg hover:scale-105 transition-transform"
+              >
+                SOS
+              </button>
+            </div>
+          </div>
         </div>
 
         <div className="ml-auto flex items-center space-x-4 pr-4">
