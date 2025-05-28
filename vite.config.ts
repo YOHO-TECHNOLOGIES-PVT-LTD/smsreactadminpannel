db87faf48d3579886d41fb96dc1b005aae98b3b0
unknown <raajeskumar.yoho@gmail.com>
2025-05-28 23:26:38 +0530

    complete2

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
-    include: ['recharts'] // <-- Force pre-bundling
+    include: ['recharts']
   },
   build: {
+    minify: false, // ✅ HERE
+    sourcemap: true,
     outDir: 'dist',
     assetsDir: 'assets',
-    chunkSizeWarningLimit: 2000, // Increase warning limit to 1MB (optional)
-     sourcemap: true, 
-     minify: false,
-
+    chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            // Split react and react-dom into their own chunk
-            if (id.includes('react')) {
-              return 'vendor_react'
-            }
-            // Split lodash into its own chunk (example)
-            if (id.includes('lodash')) {
-              return 'vendor_lodash'
-            }
-            // All other node_modules into vendor chunk
+            if (id.includes('react')) return 'vendor_react'
+            if (id.includes('lodash')) return 'vendor_lodash'
             return 'vendor'
           }
         }
