dba74e3ae700e10476499b0fa8b5538eb641be31
unknown <raajeskumar.yoho@gmail.com>
2025-05-31 21:08:14 +0530

    update4

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['recharts'],
+  },
   build: {
     minify: false,
     sourcemap: true,
