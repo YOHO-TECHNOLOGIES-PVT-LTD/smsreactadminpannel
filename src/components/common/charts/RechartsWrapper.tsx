8bb4799659b0e0f7ae2150aa63967d48bcda6dfe
unknown <raajeskumar.yoho@gmail.com>
2025-06-12 23:10:02 +0530

    update

--- src/components/common/charts/RechartsWrapper.tsx
+++ src/components/common/charts/RechartsWrapper.tsx
@@ -1,14 +1,5 @@
 // Recharts wrapper to ensure React is available before loading charts
 import '../../../react-shim'; // Ensure React shim is loaded first
-import { ReactNode } from 'react';
-
-interface RechartsWrapperProps {
-  children: ReactNode;
-}
-
-export const RechartsWrapper = ({ children }: RechartsWrapperProps) => {
-  return <>{children}</>;
-};
 
 // Re-export Recharts components with React shim loaded
 export * from 'recharts';
